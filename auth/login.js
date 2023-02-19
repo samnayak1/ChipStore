be8a2fbc1e33ea31f53160c0c3b0098160c59c22
Samasth Nayak <samasthnayak1@gmail.com>
2023-02-19 23:02:30 +0530

    hack fix

--- auth/login.js
+++ auth/login.js
@@ -5,7 +5,8 @@ const router=express.Router();
 router.post('/',async(req,res)=>{
  
     const loginCredentials={email,password}=req.body;
-    const user=await pool.query("SELECT * from usertable where ")
+    const user=await pool.query("SELECT email,password,isactive from usertable where email=$1",[email]);
+    console.log(user); 
    
 
 
