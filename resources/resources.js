120854234e42047a03791f541c0640b0daed5f60
Samasth Nayak <samasthnayak1@gmail.com>
2023-03-06 22:48:12 +0530

    changes gitignore to include settings.json

--- resources/resources.js
+++ resources/resources.js
@@ -11,7 +11,7 @@ const fs=require("fs");
 router.get('/category/:categoryname',async(req,res)=>{
   const category=req.params.categoryname;
   
-  const categorytablequery=await pool.query("SELECT a.imagename,b.name,b.price,b.discount,b.priceafterdiscount,b.created_time,b.seller_id from imagetable a RIGHT JOIN producttable b on a.product_id=b.product_id where category=$1 AND a.type=$2 limit 5",[category,'avatar']);
+  const categorytablequery=await pool.query("SELECT a.product_id,a.imagename,b.name,b.price,b.discount,b.priceafterdiscount,b.created_time,b.seller_id from imagetable a RIGHT JOIN producttable b on a.product_id=b.product_id where category=$1 AND a.type=$2 limit 5",[category,'avatar']);
   const categoryrows=categorytablequery.rows;
   
   res.status(201).json(categoryrows);
