91c7a526f7546a947be11a9ef9691597f4ddfde6
Samasth Nayak <samasthnayak1@gmail.com>
2023-03-09 19:40:41 +0530

    changes to client

--- client/src/Views/Home.js
+++ client/src/Views/Home.js
@@ -2,15 +2,29 @@ import React from 'react'
 
 import ProductList from './ProductList';
 import useSWR from 'swr'
-
+import FetchData from '../FetchData';
 function Home() {
   const fetcher=(...args)=>fetch(...args).then((res)=>res.json())
- const {data,error}=useSWR("http://localhost:5001/resources/trending/5/1",fetcher,{suspense:true})
-   
+// const {data,error}=useSWR("http://localhost:5001/resources/trending/5/1",fetcher,{suspense:true})
+  const data=FetchData("http://localhost:5001/resources/trending/5/1")
   return (
     <div>
-      {error?<h1>there was an error</h1>:null}
-     {data?<ProductList data={data}/>:<div>data not found</div>}
+      <h1>Trending this month</h1>
+
+
+      { data.map((product)=>
+  <li key={product.product_id}>
+   <h3>{product.name}</h3>
+   <h5>Original Price :{product.price}</h5>
+   <h5>Price after discount: {product.priceafterdiscount}</h5>
+
+   <img src={product.imagename} height={50} width={50}/>
+   
+  </li>
+  
+  )
+}
+     
        
     </div>
   )
@@ -19,6 +33,18 @@ function Home() {
 export default Home
 
 //  {error?<h1>there was an error</h1>:null}
-//{data?<ProductList data={data}/>:<div>data not found</div>}
+//{data?<ProductList data={[data]}/>:<div>data not found</div>}
 
+/* 
+{ data.map((product)=>
+  <li key={product.product_id}>
+   <h3>{product.name}</h3>
+   <h5>Original Price :{product.price}</h5>
+   <h5>Price after discount: {product.priceafterdiscount}</h5>
 
+   <img src={product.imagename} height={50} width={50}/>
+   
+  </li>
+  
+  )
+} */
--- client/src/Views/ProductList.js
+++ client/src/Views/ProductList.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
-function ProductList(data) {
+function ProductList([data]) {
   console.log(data);
   return (
     <div>
           <h2>Trending this month</h2>
           {
+          data.map((product)=>
+           <li key={product.product_id}>
+            <h1>{product.name}</h1>
+            <h1>{product.price}</h1>
+           </li>
+           
+           )
               
              
-             }    
+         }    
 
           
       
@@ -19,8 +26,3 @@ function ProductList(data) {
 export default ProductList
 
 
-/* data instanceof Array?data.map((product)=>
-           <li key={product.product_id}>
-            <h1>{product.name}</h1>
-            <h1>{product.price}</h1>
-           </li>):<h2>hello</h2> */
